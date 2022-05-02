729cace9c3c81e731e8b5fbb67b74121c9b21859
Jeffrey B. O'Connell <jeff.oconnell@alumni.iu.edu>
2022-05-02 17:48:02 -0400

    End of Section 7

--- DatingApp/API/Startup.cs
+++ DatingApp/API/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using API.Data;
+using API.Middleware;
 using DatingApp.API.Extensions;
 using DatingApp.API.Interfaces;
 using DatingApp.API.Services;
@@ -49,17 +50,14 @@ namespace API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPIv5 v1"));
-            }
+            app.UseMiddleware<ExceptionMiddleware>();
 
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
             app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+            
             app.UseAuthentication();
             app.UseAuthorization();
 
